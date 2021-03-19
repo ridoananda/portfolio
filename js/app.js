9d9c8eef8056871a1103c361275cd819d95e9297
ridoananda <77533351+ridoananda@users.noreply.github.com>
2021-03-19 09:38:24 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,64 @@
+$(function () {
+  $('.navbar-brand span').fadeIn(1000);
+  $('.navbar-brand span').removeClass('d-none');
+
+  setTimeout(() => {
+    $('#hero-1 .btn-email').addClass('fade');
+  }, 1500);
+  $('.animate').each(function (i) {
+    setTimeout(() => {
+      $('.animate').eq(i).addClass('fade');
+    }, 200 * i);
+  });
+  let offsetTentang = $('#tentang').offset().top;
+  let offsetProjek = $('#projek').offset().top;
+  $(window).scroll(function () { 
+    let scrollTop = $(this).scrollTop();
+    if (scrollTop > offsetTentang - 350) {
+      
+      $('#tentang img').addClass('fade');
+      $('.animate-1').each(function (i) {
+        setTimeout(() => {
+          $('.animate-1').eq(i).addClass('fade');
+        }, 400 * i);
+      });
+    }
+
+    if(scrollTop > offsetProjek - 200) {
+
+      $('.animate-card').each(function (i) {
+        setTimeout(() => {
+          $('.animate-card').eq(i).addClass('fade');
+        }, 800 * i);
+      });
+    }
+    if (scrollTop > 120) {
+      $('nav.navbar').removeClass('bg-transparent').addClass('border-bottom bg-putih');
+    } else {
+      $('nav.navbar').addClass('bg-transparent').removeClass('bg-putih border-bottom');
+    }
+  });
+  $('nav a.nav-link').click(function (e) { 
+    $('button.navbar-toggler').addClass('collapsed');
+    $('nav button div').removeClass('change');
+    $('#navbarNav').removeClass('show');
+    $('nav a.nav-link').removeClass('active');
+    $(this).addClass('active');
+  });
+  
+  $('.navbar-toggler').click(function (e) { 
+    e.preventDefault();
+    $('nav button div').toggleClass('change');
+  });
+
+  $(document).on('click', 'a.nav-link',function (e) { 
+    $("html, body")
+    .stop()
+    .animate({
+        scrollTop: $($(this).attr("href")).offset().top - 40,
+      },
+      800
+      );
+    e.preventDefault();
+  });
+});

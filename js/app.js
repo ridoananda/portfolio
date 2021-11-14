e50fdc67b51b042db3e23d05d29b3e3e388a9d14
Rido Ananda <ridoananda123@gmail.com>
2021-11-14 19:48:46 +0700

    adding another project and change language to english

--- js/app.js
+++ js/app.js
@@ -1,64 +1,69 @@
 $(function () {
-  $('.navbar-brand span').fadeIn(1000);
-  $('.navbar-brand span').removeClass('d-none');
+  const date = new Date()
+  $('#date').html(date.getFullYear())
+  $('.navbar-brand span').fadeIn(1000)
+  $('.navbar-brand span').removeClass('d-none')
 
   setTimeout(() => {
-    $('#hero-1 .btn-email').addClass('fade');
-  }, 1500);
+    $('#hero-1 .btn-email').addClass('fade')
+  }, 1500)
   $('.animate').each(function (i) {
     setTimeout(() => {
-      $('.animate').eq(i).addClass('fade');
-    }, 200 * i);
-  });
-  let offsetTentang = $('#tentang').offset().top;
-  let offsetProjek = $('#projek').offset().top;
-  $(window).scroll(function () { 
-    let scrollTop = $(this).scrollTop();
+      $('.animate').eq(i).addClass('fade')
+    }, 200 * i)
+  })
+  let offsetTentang = $('#tentang').offset().top
+  let offsetProjek = $('#projek').offset().top
+  $(window).scroll(function () {
+    let scrollTop = $(this).scrollTop()
     if (scrollTop > offsetTentang - 350) {
-      
-      $('#tentang img').addClass('fade');
+      $('#tentang img').addClass('fade')
       $('.animate-1').each(function (i) {
         setTimeout(() => {
-          $('.animate-1').eq(i).addClass('fade');
-        }, 400 * i);
-      });
+          $('.animate-1').eq(i).addClass('fade')
+        }, 400 * i)
+      })
     }
 
-    if(scrollTop > offsetProjek - 200) {
-
+    if (scrollTop > offsetProjek - 200) {
       $('.animate-card').each(function (i) {
         setTimeout(() => {
-          $('.animate-card').eq(i).addClass('fade');
-        }, 800 * i);
-      });
+          $('.animate-card').eq(i).addClass('fade')
+        }, 800 * i)
+      })
     }
     if (scrollTop > 120) {
-      $('nav.navbar').removeClass('bg-transparent').addClass('border-bottom bg-putih');
+      $('nav.navbar')
+        .removeClass('bg-transparent')
+        .addClass('border-bottom bg-putih')
     } else {
-      $('nav.navbar').addClass('bg-transparent').removeClass('bg-putih border-bottom');
+      $('nav.navbar')
+        .addClass('bg-transparent')
+        .removeClass('bg-putih border-bottom')
     }
-  });
-  $('nav a.nav-link').click(function (e) { 
-    $('button.navbar-toggler').addClass('collapsed');
-    $('nav button div').removeClass('change');
-    $('#navbarNav').removeClass('show');
-    $('nav a.nav-link').removeClass('active');
-    $(this).addClass('active');
-  });
-  
-  $('.navbar-toggler').click(function (e) { 
-    e.preventDefault();
-    $('nav button div').toggleClass('change');
-  });
+  })
+  $('nav a.nav-link').click(function (e) {
+    $('button.navbar-toggler').addClass('collapsed')
+    $('nav button div').removeClass('change')
+    $('#navbarNav').removeClass('show')
+    $('nav a.nav-link').removeClass('active')
+    $(this).addClass('active')
+  })
 
-  $(document).on('click', 'a.nav-link',function (e) { 
-    $("html, body")
-    .stop()
-    .animate({
-        scrollTop: $($(this).attr("href")).offset().top - 40,
-      },
-      800
-      );
-    e.preventDefault();
-  });
-});
+  $('.navbar-toggler').click(function (e) {
+    e.preventDefault()
+    $('nav button div').toggleClass('change')
+  })
+
+  $(document).on('click', 'a.nav-link', function (e) {
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: $($(this).attr('href')).offset().top - 40,
+        },
+        800,
+      )
+    e.preventDefault()
+  })
+})
